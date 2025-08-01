ac5bd56ed7a6c3508671592522fde13d2750b0d5
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-01 13:28:16 +0530

    seperated out navbar into its own component in Navbar.jsx

--- personal-website-frontend/src/App.jsx
+++ personal-website-frontend/src/App.jsx
@@ -1,43 +1,12 @@
 import './App.css'
+import {BrowserRouter, Routes, Route, useParams, Navigate} from "react-router-dom"
+import Home from './components/Pages/Home'
 
 function App() {
 
   return (
     <>
-      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src="/Eo_circle_blue_letter-e.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-           Elizabeth Clawson
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="./components/Pages/Home.jsx">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="./components/Pages/SoftwareEngineer.jsx">Software Engineer</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Musician</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Educator</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Administrator</a>
-              </li>
-              {/* <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li> */}
-              {/* I think the above commented out line is just an example of how to disable a menu link so I will keep it for now and comment it out and delete it at the end */}
-            </ul>
-          </div>
-        </div>
-      </nav>
+      
     </>
   )
 }
