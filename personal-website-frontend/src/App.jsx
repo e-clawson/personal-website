f20568985ecd0a421e8b53de712837180a159cec
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-01 11:40:21 +0530

    added logo bar and general text to check website working

--- personal-website-frontend/src/App.jsx
+++ personal-website-frontend/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
   return (
     <>
-    
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+          Bootstrap
+          </a>
+        </div>
+      </nav>
+
+      <h1>Hello!</h1>
     </>
   )
 }
