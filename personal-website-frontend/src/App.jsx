3c9bdef49f6d7d914c57bdffa9523ec5d6ad942e
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-01 11:58:37 +0530

    added sections to navbar

--- personal-website-frontend/src/App.jsx
+++ personal-website-frontend/src/App.jsx
@@ -14,6 +14,29 @@ function App() {
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
           </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Software Engineer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Musician</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Educator</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Administrator</a>
+              </li>
+              {/* <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li> */}
+              {/* I think the above commented out line is just an example of how to disable a menu link so I will keep it for now and comment it out and delete it at the end */}
+            </ul>
+          </div>
         </div>
       </nav>
 
