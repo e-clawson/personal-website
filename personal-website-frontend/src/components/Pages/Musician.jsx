80cbf6e8681cb5f20ed06d7c04fdfe7ae30dfca0
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-14 14:38:08 +0530

    added description section and gallery section to musician page

--- personal-website-frontend/src/components/Pages/Musician.jsx
+++ personal-website-frontend/src/components/Pages/Musician.jsx
@@ -2,8 +2,63 @@ export default function Musician() {
 
 return (
     <>
-        <div>
+        <div className="title">
             <h1>Musician</h1>
+        </div>
+        <div className="description-section">
+            <p>
+                As a child, I was inspired to play bass by a rousing performance of Camille Saint-Saen's The Elephant, and I have been playing ever since. 
+                I studied Music Education at the University of Wisconsin-Madison under the instruction of Richard Davis, before pursuing my Master's in Music Performance from the University 
+                of Arizona under the instriction of Dr. Philip Alejo.
+            </p>
+            <p>
+                I have performed on upright and electric bass in a variety of settings and genres including clasical music, chamber music, and operas with orchestras, hip-hop, Jazz, bluegrass and more with small bands and ensembles, to musicals, marching bands, and more. 
+            </p>
+        </div>
+        <div className="gallery-section">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                    />
+
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Wintry Mountain Landscape"
+                    />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Mountains in the Clouds"
+                    />
+
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                    />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Waves at Sea"
+                    />
+
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Yosemite National Park"
+                    />
+                </div>
+            </div> 
         </div>
     </>
 )
