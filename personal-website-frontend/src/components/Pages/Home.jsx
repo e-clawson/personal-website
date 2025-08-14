59ebc746fe916c21048fcdf92cb0d6b5164419d4
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-14 13:01:29 +0530

    added styling to buttons

--- personal-website-frontend/src/components/Pages/Home.jsx
+++ personal-website-frontend/src/components/Pages/Home.jsx
@@ -4,28 +4,37 @@ export default function Home(){
 
 return (
 <>
-    <div>
+    <div className='homepage-greeting'>
         <h1> Hello, Welcome to my Site!</h1>
     </div>
     <div >
         <img className="headshot" src="/headshot.jpg" alt="Image of Liz with her bass"/>
     </div>
-    <div> 
-        <h2>About Me: </h2>
-    </div>
-    <div>
-        <p>I am an emerging software engineer with proficiency in full-stack engineering, transitioning from a decade-long career as a musician, arts administrator, and educator. 
+    <div className='link-section'>
+        <a className='links' href="https://www.linkedin.com/in/elizabeth-clawson/" >LinkedIn</a>
+        <a className='links' href="https://github.com/e-clawson" >GitHub</a>
 
-        In 2022, I completed a 10-month, part-time software engineering bootcamp with Flatiron School, where I developed a portfolio of projects and gained proficiency in full-stack engineering. Recently, I completed an immersive, 16-week, full-time software engineering bootcamp focused on the MERN stack through Per Scholas. I am excited to delve deeper into full-stack engineering technologies and principles, continue advancing my education in software engineering, and find a role where I can use  my skills to make meaningful contributions to a collaborative and dynamic team.
-
-        After witnessing fellow musicians and arts administrators successfully transition to fulfilling tech careers I was inspired to explore software engineering myself, and I have found that the interconnectedness of various fields with digital technology excites me and my passion for creative problem-solving and crafting meaningful solutions makes me come back to software engineering wanting to learn more! 
-
-        In my previous roles, I have successfully managed diverse teams and communicated effectively across disciplines with customers, teams, partners, and stakeholders. At the Chinese Fine Arts Society, I collaborated with numerous arts organizations and individuals to create impactful arts experiences at iconic locations across Chicagoland, including the Field Museum, Navy Pier, Maggie Daley Park, and the Chicago Cultural Center. As an educator, I worked with the Richard Davis Foundation for Young Bassists, the University of Arizona String Project, and Cochise College, providing musicians of all ages with a strong foundation in string performance.
-        </p>
     </div>
-    <div>
-        <h3>Please feel free to check out my website and learn a little more about me!</h3>
+    <div className='homepage-text'>
+        <div> 
+            <h2>About Me: </h2>
+        </div>
+        <div>
+            <p>I am an emerging software engineer with proficiency in full-stack engineering, transitioning from a decade-long career as a musician, arts administrator, and educator. 
+
+            In 2022, I completed a 10-month, part-time software engineering bootcamp with Flatiron School, where I developed a portfolio of projects and gained proficiency in full-stack engineering. Recently, I completed an immersive, 16-week, full-time software engineering bootcamp focused on the MERN stack through Per Scholas. I am excited to delve deeper into full-stack engineering technologies and principles, continue advancing my education in software engineering, and find a role where I can use  my skills to make meaningful contributions to a collaborative and dynamic team.
+            </p>
+            <p>After witnessing fellow musicians and arts administrators successfully transition to fulfilling tech careers I was inspired to explore software engineering myself, and I have found that the interconnectedness of various fields with digital technology excites me and my passion for creative problem-solving and crafting meaningful solutions makes me come back to software engineering wanting to learn more! 
+
+            In my previous roles, I have successfully managed diverse teams and communicated effectively across disciplines with customers, teams, partners, and stakeholders. At the Chinese Fine Arts Society, I collaborated with numerous arts organizations and individuals to create impactful arts experiences at iconic locations across Chicagoland, including the Field Museum, Navy Pier, Maggie Daley Park, and the Chicago Cultural Center. As an educator, I worked with the Richard Davis Foundation for Young Bassists, the University of Arizona String Project, and Cochise College, providing musicians of all ages with a strong foundation in string performance.
+            </p>
+        </div>
+        <div>
+            <h3>Please feel free to check out my website and learn a little more about me!</h3>
+        </div>
+
     </div>
+    
 </>
 
 )
