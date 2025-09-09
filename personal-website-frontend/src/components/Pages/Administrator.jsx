fcbcc80e1fd5014b3148c1f775507fa1bea1fb0d
Elizabeth Clawson <emclawson1@gmail.com>
2025-09-09 11:35:53 +0530

    added overview and teaching statement to educator page

--- personal-website-frontend/src/components/Pages/Administrator.jsx
+++ personal-website-frontend/src/components/Pages/Administrator.jsx
@@ -18,6 +18,8 @@ return (
                 Throughout my career, I have overseen the production of hundreds of concerts, workshops, and public events, recruiting and leading teams of over 100+ volunteers, building and maintaining relationships with dozens of partner organizations, in order to create stunning, engaging events for thousands of people. 
             </p>
         </div>
+
+        
     </div>
 )
 }
--- personal-website-frontend/src/components/Pages/Educator.jsx
+++ personal-website-frontend/src/components/Pages/Educator.jsx
@@ -2,10 +2,22 @@ export default function Educator() {
 
 
 return (
-    <>
-        <div>
-            <h1>Educator</h1>
-        </div>
-    </>
+    <div className='page'>
+    <div>
+        <h1>Educator</h1>
+    </div>
+    <div>
+        <p>
+            I have a background in education, having received my bachelors degree in Instrumental  Music Education from the University of Wisconsin — Madison. I have worked with learners ages 2.5 - 75 with a verity of backgrounds and experiences, in all types of settings from private lessons, to group classes and workshops. Additionally, throughout my arts administration career, I have worked in support of many educational environments, from planning and executing music festivals, competitions, and youth music symposiums, to developing and teaching a curriculum for educators to learn how to teach the basics of double bass to their school orchestra students. 
+        </p>
+    </div>
+    <div>
+        <h1>Teaching Statement</h1>
+        <p>
+        As an educator, I am committed to supporting each student as an individual, collaborating with them to ensure growth by aquiring necessary skills for success while nurturing their excitement, curiosity, and independence surrounding music. As a private music lesson instructor, I ask all students to share three goals with me so that we can cultivate an agreed upon curriculum incorporating their interests. While working with the University of Arizona string project with very young learners, we took a collaborative approach to learning; in addition to learning how to hold their instruments and learning first notes, we explored different sounds we could make on our instruments, wrote a class story together, and used our new sounds to create a song to accompany the class story. Even the youngest of learners was able to get excited about learning through this collaborative approach and have agency during the learning process. Additionally, I have a deep practice of incorporating journaling into individual student lessons. Through journals, I can write down what we worked on each week in our lessons, and we can set a practice plan together for the week. Students are also encouraged to write down a schedule of what they did each day in practice, for their own memory and tracking, and to write down any questions that arise while practicing, or to write down if they experience any pain or discomfort while playing. It is so easy to forget what you worked on and this way we can ensure we’re getting the most out of our lessons while I help them to grow the skills for independent self-reflection and the eventual ability to teach themselves without me. 
+        </p>
+    </div>
+    
+</div>
 )
 }
