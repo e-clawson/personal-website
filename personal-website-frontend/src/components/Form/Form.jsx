66940d7010d9a832c36f06ed9a148ba9c7433c20
Elizabeth Clawson <emclawson1@gmail.com>
2025-09-09 14:14:32 +0530

    added the last name input box

--- personal-website-frontend/src/components/Form/Form.jsx
+++ personal-website-frontend/src/components/Form/Form.jsx
@@ -7,7 +7,11 @@ const Form = () => {
             <h5>Get In Touch:</h5>
             <div className="input-box">
                 <label>First Name</label>
-
+                <input type="text" className="field" placeholder='Enter Your First Name' required/>
+            </div>
+            <div className="input-box">
+                <label>Last Name</label>
+                <input type="text" className="field" placeholder='Enter Your Last Name' required/>
             </div>
         </form>
 
