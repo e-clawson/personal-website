82f9ffa87323887e8a836a0b077358898ecf8250
Elizabeth Clawson <emclawson1@gmail.com>
2025-09-09 14:17:48 +0530

    added the input for the message into cnotact form

--- personal-website-frontend/src/components/Form/Form.jsx
+++ personal-website-frontend/src/components/Form/Form.jsx
@@ -6,16 +6,20 @@ const Form = () => {
         <form>
             <h5>Get In Touch:</h5>
             <div className="input-box">
-                <label>First Name</label>
+                <label>First Name: </label>
                 <input type="text" className="field" placeholder='Enter Your First Name' required/>
             </div>
             <div className="input-box">
-                <label>Last Name</label>
+                <label>Last Name: </label>
                 <input type="text" className="field" placeholder='Enter Your Last Name' required/>
             </div>
             <div className="input-box">
-                <label>Email Address</label>
+                <label>Email Address: </label>
                 <input type="email" className="field" placeholder='Enter Your Email Address' required/>
+            </div>
+            <div className="input-box">
+                <label>Message: </label>
+                <textarea name="" id="" placeholder="Please enter your message here." required></textarea>
             </div>
         </form>
 
