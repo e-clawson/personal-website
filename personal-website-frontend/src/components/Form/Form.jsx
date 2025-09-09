ec0095e0f435fc8f294b6505397cf23840b146f1
Elizabeth Clawson <emclawson1@gmail.com>
2025-09-09 14:15:43 +0530

    added email address input field tocontact form

--- personal-website-frontend/src/components/Form/Form.jsx
+++ personal-website-frontend/src/components/Form/Form.jsx
@@ -13,6 +13,10 @@ const Form = () => {
                 <label>Last Name</label>
                 <input type="text" className="field" placeholder='Enter Your Last Name' required/>
             </div>
+            <div className="input-box">
+                <label>Email Address</label>
+                <input type="email" className="field" placeholder='Enter Your Email Address' required/>
+            </div>
         </form>
 
     </section>
