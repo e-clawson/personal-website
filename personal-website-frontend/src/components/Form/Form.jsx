73cd472adc185cb0d01cefa5dd37d5f27dbefd23
Elizabeth Clawson <emclawson1@gmail.com>
2025-09-09 20:13:13 +0530

    got footer form to work:

--- personal-website-frontend/src/components/Form/Form.jsx
+++ personal-website-frontend/src/components/Form/Form.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import './form.css'
 
 const Form = () => {
+    const MY_ACCESS_KEY = import.meta.env.VITE_API_KEY;
+    const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
+    
     const onSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
     
-        formData.append("access_key", "MY_ACCESS_KEY");
+        formData.append("access_key", MY_ACCESS_KEY);
     
         const object = Object.fromEntries(formData);
         const json = JSON.stringify(object);
     
-        const res = await fetch("https://api.web3forms.com/submit", {
+        const res = await fetch(VITE_BASE_URL, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -27,23 +30,23 @@ const Form = () => {
 
   return (
     <section className='contact-form'>
-        <form>
+        <form onSubmit={onSubmit}>
             <h5>Get In Touch:</h5>
             <div className="input-box">
                 <label>First Name: </label>
-                <input type="text" className="field" placeholder='Enter Your First Name' required/>
+                <input type="text" className="field" placeholder='Enter Your First Name' name="firstName" required/>
             </div>
             <div className="input-box">
                 <label>Last Name: </label>
-                <input type="text" className="field" placeholder='Enter Your Last Name' required/>
+                <input type="text" className="field" placeholder='Enter Your Last Name' name="lastName" required/>
             </div>
             <div className="input-box">
                 <label>Email Address: </label>
-                <input type="email" className="field" placeholder='Enter Your Email Address' required/>
+                <input type="email" className="field" placeholder='Enter Your Email Address' name="email" required/>
             </div>
             <div className="input-box">
                 <label>Message: </label>
-                <textarea name="" id="" className="field message" placeholder="Please enter your message here." required></textarea>
+                <textarea name="message" id="" className="field message" placeholder="Please enter your message here." required></textarea>
             </div>
             <button type="submit">Submit</button>
         </form>
